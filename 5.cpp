304fa03f6b9ec55e0c0e22d40fc8e44f6b3f3754
Katerina <amisto@yandex.ru>
2021-02-05 19:17:10 +0300

    ftgsdftgesgr

---
+++ 5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    unsigned nz, nl;
+    cin >>nz >>nl;
+    unsigned *pz = new bool[nz];
+    for (int i = 0; i < nz; i++)
+        cin >>pz[i];
+    bool* z = new bool[nz];
+
+
+
+    cout <<prime(n) <<endl;
+    delete z;
+    return 0;
+}
